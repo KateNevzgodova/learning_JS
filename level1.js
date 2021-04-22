81ba4d075a18ebf97e0dea39c6e02a23a6581912
KateNevzgodova <ekaterinanevzgodova@seznam.cz>
2021-04-22 20:01:44 +0200

    timers

--- level1.js
+++ level1.js
@@ -56,3 +56,63 @@ function showSlides3(a) {
     slides3[slideIndex3 - 1].style.display = "block";
     dots[slideIndex3 - 1].className += " active";
 }
+
+function goTimer() {
+    window.timerId = window.setInterval(timer, 1000);
+}
+
+function stopTimer() {
+    window.clearInterval(window.timerId);
+}
+
+function timer() {
+    let elem = document.getElementById('test');
+    elem.value = parseInt(elem.value) + 1;
+}
+
+function go() {
+    window.timerId = window.setInterval(timer, 1000);
+    document.getElementById('go').disabled = true;
+}
+
+function go1() {
+    window.timerId = window.setInterval(timer2, 1000);
+    document.getElementById('go1').disabled = true;
+    document.getElementById('stop').disabled = false;
+}
+
+function stop() {
+    window.clearInterval(window.timerId);
+    document.getElementById('go1').disabled = false;
+    document.getElementById('stop').disabled = true;
+}
+
+function timer2() {
+    var img1 = document.getElementById('img1');
+    var img2 = document.getElementById('img2');
+    var img3 = document.getElementById('img3');
+    var img4 = document.getElementById('img4');
+    var img5 = document.getElementById('img5');
+    var img6 = document.getElementById('img6');
+    var tmp = img1.src;
+    img1.src = img2.src;
+    img2.src = img3.src;
+    img3.src = img4.src;
+    img4.src = img5.src;
+    img5.src = img6.src;
+    img6.src = tmp;
+}
+let elements = document.getElementsByTagName('input');
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('blur', func);
+}
+
+function func() {
+    let correctLength = this.dataset.length; //правильное количество
+    let inputDataLength = this.value.length; //вбитое в инпут количество
+    if (correctLength == inputDataLength) {
+        this.style.borderColor = 'green';
+    } else {
+        this.style.borderColor = 'red';
+    }
+}
